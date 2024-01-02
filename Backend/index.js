193a2f4ed1d087259e4f7d3a9896a902fe44956c
Tunir Wabhitkar <smokeinjone@gmail.com>
2024-01-02 21:31:53 +0530

    Add files via upload

---
+++ Backend/index.js
@@ -0,0 +1,49 @@
+const express = require("express");
+const cors = require("cors");
+const axios = require("axios");
+
+const app = express();
+app.use(express.json());
+app.use(cors({ origin: true }));
+
+const CHAT_ENGINE_PROJECT_ID = "6441dc56-60d3-4e1a-8f8e-b6ccb80ff039";
+const CHAT_ENGINE_PRIVATE_KEY = "ce1caf07-25dd-4dd4-a450-6201b5015f4d";
+
+app.post("/signup", async (req, res) => {
+  const { username, secret, email, first_name, last_name } = req.body;
+
+  // Store a user-copy on Chat Engine!
+  // Docs at rest.chatengine.io
+  try {
+    const r = await axios.post(
+      "https://api.chatengine.io/users/",
+      { username, secret, email, first_name, last_name },
+      { headers: { "Private-Key": "ce1caf07-25dd-4dd4-a450-6201b5015f4d" } }
+    );
+    return res.status(r.status).json(r.data);
+  } catch (e) {
+    return res.status(e.response.status).json(e.response.data);
+  }
+});
+
+app.post("/login", async (req, res) => {
+  const { username, secret } = req.body;
+
+  // Fetch this user from Chat Engine in this project!
+  // Docs at rest.chatengine.io
+  try {
+    const r = await axios.get("https://api.chatengine.io/users/me/", {
+      headers: {
+        "Project-ID": "6441dc56-60d3-4e1a-8f8e-b6ccb80ff039",
+        "User-Name": username,
+        "User-Secret": secret,
+      },
+    });
+    return res.status(r.status).json(r.data);
+  } catch (e) {
+    return res.status(e.response.status).json(e.response.data);
+  }
+});
+
+// vvv On port 3001!
+app.listen(3001);
---
+++ Frontend/src/ChatsPage.jsx
@@ -0,0 +1,16 @@
+import { PrettyChatWindow } from "react-chat-engine-pretty";
+
+const ChatsPage = (props) => {
+  return (
+    <div style={{ height: "100vh", width: "100vw" }}>
+      <PrettyChatWindow
+        projectId={'6441dc56-60d3-4e1a-8f8e-b6ccb80ff039'}
+        username={props.user.username}
+        secret={props.user.secret}
+        style={{ height: "100%" }}
+      />
+    </div>
+  );
+};
+
+export default ChatsPage;
